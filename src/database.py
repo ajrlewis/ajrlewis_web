fcfca565df2a138840a60ead34c6765829113d2b
ajrlewis <hello@ajrlewis.com>
2024-10-29 12:57:52 +0000

    Removed ValueError exception and place default database URL for SQLAlchemy.

--- src/database.py
+++ src/database.py
@@ -5,11 +5,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "mysql+pymysql://")
 if not SQLALCHEMY_DATABASE_URL:
     message = f"SQLALCHEMY_DATABASE_URL not set, unable to make SessionLocal object."
     logger.error(message)
-    raise ValueError(message)
+    # raise ValueError(message)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
