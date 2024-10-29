b40a578a9199ed8cfa7154b8464cb7d4a8ee2855
ajrlewis <hello@ajrlewis.com>
2024-10-29 19:48:42 +0000

    Added flask extensions.

--- src/app.py
+++ src/app.py
@@ -3,18 +3,17 @@ import sys
 
 from flask import Flask
 
-# from flask_migrate import Migrate
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_wtf.csrf import CSRFProtect
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 from loguru import logger
 
-# csrf = CSRFProtect()
-# db = SQLAlchemy()
-# migrate = Migrate()
+csrf = CSRFProtect()
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(Config) -> Flask:
-    # template_folder = os.path.abspath(__file__)
     template_folder = "../templates"
     static_folder = "../static"
     logger.debug(f"{template_folder = } {static_folder = }")
@@ -24,14 +23,13 @@ def create_app(Config) -> Flask:
         static_folder=static_folder,
         template_folder=template_folder,
     )
-
     logger.debug(f"{app = }")
 
     app.config.from_object(Config)
 
-    # csrf.init_app(app=app)
-    # db.init_app(app=app)
-    # migrate.init_app(app=app, db=db)
+    csrf.init_app(app=app)
+    db.init_app(app=app)
+    migrate.init_app(app=app, db=db)
 
     with app.app_context():
         from blueprints.index_bp import index_bp
