189833e5d6d4a10bdd7c84e955ba92bf16fe51d3
ajrlewis <hello@ajrlewis.com>
2024-10-29 14:42:40 +0000

    Added wait time to ASGIMiddleware

--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,12 @@ import os
 import sys
 
 from a2wsgi import ASGIMiddleware
+from loguru import logger
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
 
 from main import app
 
-application = ASGIMiddleware(app)
+logger.debug(f"{app = }")
+
+application = ASGIMiddleware(app, wait_time=5.0)
