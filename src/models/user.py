47d4b1c954996bbb5bccd47a8261181f979c011c
ajrlewis <hello@ajrlewis.com>
2024-10-31 16:23:27 +0000

    Added length to string columns for 'VARCHAR requires a length on dialect mysql' error

--- src/models/user.py
+++ src/models/user.py
@@ -6,7 +6,9 @@ from app import db
 
 class User(db.Model):
     user_id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(unique=True)
-    password_hash: Mapped[str] = mapped_column()
-    api_key: Mapped[str] = mapped_column()
+    name: Mapped[str] = mapped_column(db.String(16), unique=True)
+    password_hash: Mapped[str] = mapped_column(db.String(102), nullable=True)
+    api_key: Mapped[str] = mapped_column(
+        db.String(46)
+    )  # "sk_" + secrets.token_urlsafe(32)
     is_admin: Mapped[bool] = mapped_column(default=False)
