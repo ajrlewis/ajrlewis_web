17715a6a4a288fa26bb28ff2b6db343115929921
ajrlewis <hello@ajrlewis.com>
2024-10-31 17:04:47 +0000

    Added create_api_key in user_service.

--- src/models/user.py
+++ src/models/user.py
@@ -5,7 +5,5 @@ class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(16), nullable=False)
     password_hash = db.Column(db.String(102), nullable=True)
-    api_key = db.Column(
-        db.String(46), nullable=False
-    )  # "sk_" + secrets.token_urlsafe(32)
+    api_key = db.Column(db.String(46), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,3 +1,4 @@
+import secrets
 from typing import Optional
 
 from loguru import logger
@@ -10,3 +11,8 @@ def get_user_from_api_key(api_key: str) -> Optional[User]:
     user = db.session.query(User).filter_by(api_key=api_key).first()
     logger.debug(f"{user = }")
     return user
+
+
+def create_api_key() -> str:
+    api_key = "sk_" + secrets.token_urlsafe(32)
+    return api_key
